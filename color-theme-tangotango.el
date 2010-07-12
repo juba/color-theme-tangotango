7efa40c8f5dff483a2d83c85b70e7b37dcab5fba
Juba <julien@nozav.org>
2010-07-12 11:23:24 +0200

    License added

--- color-theme-tangotango.el
+++ color-theme-tangotango.el
@@ -1,10 +1,37 @@
-;;; Color theme based on the Tango Palette colors. 
-;;; First derived from the color-theme-tango.el color theme created by danranx@gmail.com :
+;;; Emacs Color theme based on the Tango Palette colors. 
+;;; First derived from color-theme-tango.el,  created by danranx@gmail.com :
 ;;; http://www.emacswiki.org/emacs/color-theme-tango.el
 
+;; Copyright (C) 2010 Julien Barnier <julien@nozav.org>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 2, or (at your option) any later
+;; version.
+
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with Emacs; see the file COPYING, or type `C-h C-c'. If not,
+;; write to the Free Software Foundation at this address:
+
+;;   Free Software Foundation
+;;   51 Franklin Street, Fifth Floor
+;;   Boston, MA 02110-1301
+;;   USA
+
+;;; Code:
+
+(eval-when-compile
+  (require 'color-theme))
 
 (defun color-theme-tangotango ()
-  "A color theme based on Tango Palette."
+  "A color theme based on Tango Palette colors."
   ;; Color codes :
   ;; - blue :       "dodger blue"
   ;; - yellow :     "#edd400"

7eedc61e079682d81e718090d480744152a3da2f
Julien Barnier <julien@nozav.org>
2013-07-15 11:26:42 +0200

    Add autoload function for ELPA install

--- tangotango-theme.el
+++ tangotango-theme.el
@@ -237,6 +237,15 @@
  )
 
 
+;; Autoload for MELPA
+
+;;;###autoload
+(and load-file-name
+     (boundp 'custom-theme-load-path)
+     (add-to-list 'custom-theme-load-path
+                  (file-name-as-directory
+                   (file-name-directory load-file-name))))
+
 (provide-theme 'tangotango)
 
 ;;; tangotango-theme.el ends here
